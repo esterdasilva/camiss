1ff319a5da46df769049e2a6b9b40c558a5ca087
esterdasilva <138500649+esterdasilva@users.noreply.github.com>
2023-07-24 11:21:51 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,21 @@
+createCanvas(600, 600);
+
+background("white");
+
+}
+
+function draw() {
+
+st
+function setup() {
+roke("blue");
+
+fill("red");
+
+if(mouseIsPressed){
+
+rect(mouseX, mouseY, 20, 35);
+
+}
+
+}
